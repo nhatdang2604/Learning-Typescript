f504dcbc4008fdc0ea6c64153b89747783ba51a8
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-09 21:31:41 +0700

    Finish chap 3

---
+++ chap_3/sandbox.js
@@ -0,0 +1,14 @@
+"use strict";
+var character = "mario";
+var age = 30;
+var isBlackBelt = false;
+//character = 20;
+character = "luigi";
+//age = "yoshi";
+age = 40;
+//isBlackBelt = "yes";
+isBlackBelt = true;
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circle(7.5));
--- chap_3/sandbox.ts
+++ chap_3/sandbox.ts
@@ -1,1 +1,20 @@
 "use strict";
+
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
+
+//character = 20;
+character = "luigi";
+
+//age = "yoshi";
+age = 40;
+
+//isBlackBelt = "yes";
+isBlackBelt = true;
+
+const circle = (diameter: number): number => {
+    return diameter * Math.PI;
+}
+
+console.log(circle(7.5));

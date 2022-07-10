a54d9463dbcf103a1354e4056dc574fc9b4b8aed
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-10 21:48:46 +0700

    finish chap 16

--- chap_16/public/app.js
+++ chap_16/public/app.js
@@ -1,11 +1,20 @@
 "use strict";
 import { Invoice } from "./classes/Invoice.js";
-let invoices = [];
-invoices.push(new Invoice("mario", "work on the mario webiste", 250));
-invoices.push(new Invoice("luigi", "work on the luigi webiste", 300));
-for (const invoice of invoices) {
-    console.log(invoice.client, invoice.format());
-}
+import { Payment } from "./classes/Payment.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 200);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// let invoices: Invoice[] = [];
+// invoices.push(new Invoice("mario", "work on the mario webiste", 250));
+// invoices.push(new Invoice("luigi", "work on the luigi webiste", 300));
+// for (const invoice of invoices) {
+//     console.log(invoice.client, invoice.format());
+// }
 //console.log(invoices[0].#_client);        //invalid
 const form = document.querySelector(".new-item-form");
 //inputs
@@ -15,5 +24,12 @@ const details = document.getElementById("details");
 const amount = document.getElementById("amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    let doc = undefined;
+    if ("invoice" === type.value.toLowerCase()) {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else if ("payment" === type.value.toLowerCase()) {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc === null || doc === void 0 ? void 0 : doc.format());
 });
--- chap_16/src/app.ts
+++ chap_16/src/app.ts
@@ -1,13 +1,26 @@
 "use strict";
 import {Invoice} from "./classes/Invoice.js";
+import {Payment} from "./classes/Payment.js";
+import {HasFormatter} from "./interfaces/HasFormatter.js";
 
-let invoices: Invoice[] = [];
-invoices.push(new Invoice("mario", "work on the mario webiste", 250));
-invoices.push(new Invoice("luigi", "work on the luigi webiste", 300));
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-for (const invoice of invoices) {
-    console.log(invoice.client, invoice.format());
-}
+// docOne = new Invoice("yoshi", "web work", 250);
+// docTwo = new Payment("mario", "plumbing work", 200);
+
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+
+// let invoices: Invoice[] = [];
+// invoices.push(new Invoice("mario", "work on the mario webiste", 250));
+// invoices.push(new Invoice("luigi", "work on the luigi webiste", 300));
+
+// for (const invoice of invoices) {
+//     console.log(invoice.client, invoice.format());
+// }
 
 //console.log(invoices[0].#_client);        //invalid
 
@@ -21,10 +34,13 @@ const amount = document.getElementById("amount") as HTMLInputElement;
 
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    console.log(
-        type.value,
-        toFrom.value,
-        details.value,
-        amount.valueAsNumber,
-    )
+
+    let doc: HasFormatter|undefined = undefined;
+    if ("invoice" === type.value.toLowerCase()) {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    } else if ("payment" === type.value.toLowerCase()) {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    } 
+
+    console.log(doc?.format());
 })

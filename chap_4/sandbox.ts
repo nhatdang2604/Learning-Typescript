949a209be3de5faec2a486c5e9d5d884da8b21a3
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-10 11:44:01 +0700

    Finish testing array

--- chap_4/sandbox.ts
+++ chap_4/sandbox.ts
@@ -1,1 +1,22 @@
 "use strict";
+
+//arrays
+let names = [
+    "luigi",
+    "mario",
+    "yoshi",
+]
+
+names.push("toad");     //valid
+//names.push(12);       //invalid
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);       //valid
+//numbers.push("shaun");//invalid
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+mixed.push("ryu");  //valid
+mixed.push(10);     //valid
+//mixed.push(true)  //invalid
